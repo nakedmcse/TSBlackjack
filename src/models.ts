83cd609754f262b3784b416d8528937376f4b8a5
Walker Aldridge <daldridge@lairdscomputer.com>
2024-09-27 08:57:24 -0500

    Fix models

--- src/models.ts
+++ src/models.ts
@@ -1,7 +1,10 @@
 // Models for blackjack API
-import {Column, Entity} from "typeorm";
+import {Column, Entity, Index} from "typeorm";
 
 @Entity()
+@Index(["token"], {
+    unique: true,
+})
 export class game {
     @Column({primary: true, type: "text", unique: true, nullable: false})
     public token: string;
@@ -25,7 +28,15 @@ export class game {
     public playerCards: string[];
 
     public constructor(token: string, device: string, status:string, startedOn: number, deck: string[] = [],
-                       dealerCards: string[] = [], playerCards: string[] = []) {}
+                       dealerCards: string[] = [], playerCards: string[] = []) {
+        this.token = token;
+        this.device = device;
+        this.status = status;
+        this.startedOn = startedOn;
+        this.deck = deck;
+        this.dealerCards = dealerCards;
+        this.playerCards = playerCards;
+    }
 
     static suits = ['\u2660','\u2663','\u2665','\u2666'];
     static faces = ['2','3','4','5','6','7','8','9','10','A','J','Q','K'];
@@ -45,19 +56,19 @@ export class game {
     }
 
     public deal(): void {
-        this.playerCards.push(this.deck.pop());
-        this.playerCards.push(this.deck.pop());
-        this.dealerCards.push(this.deck.pop());
-        this.dealerCards.push(this.deck.pop());
+        this.playerCards.push(this.deck.pop() ?? "");
+        this.playerCards.push(this.deck.pop() ?? "");
+        this.dealerCards.push(this.deck.pop() ?? "");
+        this.dealerCards.push(this.deck.pop() ?? "");
     }
 
     public hit(): void {
-        this.playerCards.push(this.deck.pop());
+        this.playerCards.push(this.deck.pop() ?? "");
     }
 
     public stay(): void {
         while(this.value(this.dealerCards) < 17) {
-            this.dealerCards.push(this.deck.pop())
+            this.dealerCards.push(this.deck.pop() ?? "")
         }
     }
 
@@ -101,7 +112,12 @@ export class stat {
     @Column({type: "integer"})
     public draws: number;
 
-    public constructor(device:string, wins: number, loses: number, draws: number) {}
+    public constructor(device:string, wins: number, loses: number, draws: number) {
+        this.device = device;
+        this.wins = wins;
+        this.loses = loses;
+        this.draws = draws;
+    }
 }
 
 export class responseMsg {
